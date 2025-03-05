6e2760e468b8e9767c63cce6b8b3e04854ad92a2
Cristina Racovita <cristina.racovita98@gmail.com>
2025-03-05 21:41:53 +0100

    create react app

---
+++ evaluation-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Toolbar from './components/toolbar/Toolbar';
+
+function App() {
+  return (
+    <div className="App">
+            <Toolbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ evaluation-app/src/components/toolbar/Toolbar.tsx
@@ -0,0 +1,9 @@
+import './Toolbar.css';
+
+function Toolbar() {
+    return (
+        <div className='toolbar'></div>
+    );
+}
+
+export default Toolbar;
